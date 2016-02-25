8a8f01eccc7e5c5f89350beff26499133e867b9c
ricardogarcia <ric.garciaf@gmail.com>
2016-02-24 21:44:36 -0500

    Zoom in Activity transition

--- app/src/main/java/com/example/ricardogarcia/topfreeapps/CategoryAdapter.java
+++ app/src/main/java/com/example/ricardogarcia/topfreeapps/CategoryAdapter.java
@@ -5,6 +5,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.Drawable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.ActivityOptionsCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -81,7 +83,9 @@ public class CategoryAdapter extends BaseAdapter {
 
                     Intent intent = new Intent(activity, Apps.class);
                     intent.putExtra(CATEGORY, categories.get(position).getName());
-                    activity.startActivity(intent);
+
+                    ActivityOptionsCompat options = ActivityOptionsCompat.makeScaleUpAnimation(v, 0, 0,v.getWidth(), v.getHeight());
+                    ActivityCompat.startActivity(activity, intent, options.toBundle());
                 }
             });
         }
